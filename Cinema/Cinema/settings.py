dccfdbb298f8b5a22ecc3e974738f7e0e3fc8e3e
malek <boulahdourabdelmalek@gmail.com>
2025-09-23 03:16:23 +0100

    edited requirements.txt

--- Cinema/Cinema/settings.py
+++ Cinema/Cinema/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-w5#2yzqcldlqpaaqyf-g1k%+qq(!l6of&965)yp5252fwh!2r&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['oviesouse-boulahdourabdelmalek2190-svl6jcvp.leapcell.dev', 'localhost']
 
 
 # Application definition
@@ -136,3 +136,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'Movies_House.MyUser'
+CSRF_COOKIE_SECURE = True
+CSRF_TRUSTED_ORIGINS = ['https://oviesouse-boulahdourabdelmalek2190-svl6jcvp.leapcell.dev']
