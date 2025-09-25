026706f643e8e2262cecab01a7160da4e002b58b
malek <boulahdourabdelmalek@gmail.com>
2025-09-25 22:53:32 +0100

    nijni

--- Cinema/Cinema/settings.py
+++ Cinema/Cinema/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-w5#2yzqcldlqpaaqyf-g1k%+qq(!l6of&965)yp5252fwh!2r&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['oviesouse-boulahdourabdelmalek2190-svl6jcvp.leapcell.dev', 'localhost']
 
